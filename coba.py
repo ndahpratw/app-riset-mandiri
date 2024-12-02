555d7e1a66d3e367b9984570f6f6cce2a0a0bbae
210411100050_Indah Pratiwi <111731081+ndahpratw@users.noreply.github.com>
2024-12-02 18:19:37 +0700

    update coba.py

--- coba.py
+++ coba.py
@@ -2,6 +2,6 @@ from joblib import load
 
 try:
     model_terbaik = load("model_terbaik.pkl")
-    print("Model berhasil dimuat:", model_terbaik)
+    st.write("Model berhasil dimuat:", model_terbaik)
 except Exception as e:
-    print(f"Error saat membuka file joblib: {e}")
+    st.write(f"Error saat membuka file joblib: {e}")
