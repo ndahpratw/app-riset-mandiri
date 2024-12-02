e16cdd9c82a790e3009f677a5c96db7a5efc23c2
210411100050_Indah Pratiwi <111731081+ndahpratw@users.noreply.github.com>
2024-12-02 18:17:22 +0700

    update coba.py

--- coba.py
+++ coba.py
@@ -1,29 +1,7 @@
-import os
-import pickle
-import streamlit as st
+from joblib import load
 
-# Mendapatkan path direktori saat ini
-current_dir = os.getcwd()
-st.write("Current Directory:", current_dir)
-
-# List file yang ada di direktori
-files = os.listdir(current_dir)
-st.write("Files in Current Directory:", files)
-
-# Membuat path ke file tertentu (misalnya, 'model_terbaik.pkl')
-file_path = os.path.join(current_dir, "model_elm_terbaik.pkl")
-st.write("Path to File:", file_path)
-
-# Mengecek keberadaan file
-if os.path.exists(file_path):
-    st.success("File ditemukan COE!")
-
-    # Membuka file pickle dan memuat model
-    try:
-        with open(file_path, "rb") as file:
-            model_terbaik = pickle.load(file)
-        st.success("Model berhasil dimuat!")
-    except Exception as e:
-        st.error(f"Error saat membuka file pickle: {e}")
-else:
-    st.error("File tidak ditemukan!")
+try:
+    model_terbaik = load("model_terbaik.pkl")
+    print("Model berhasil dimuat:", model_terbaik)
+except Exception as e:
+    print(f"Error saat membuka file joblib: {e}")
