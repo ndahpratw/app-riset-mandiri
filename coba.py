d373894578e64dd0443516befb97dacb1f25fce9
ndhpratw <bolabolabangkalan5@gmail.com>
2024-12-09 08:58:08 +0700

    pembaharuan

--- coba.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from joblib import load
-
-try:
-    model_terbaik = load("model_terbaik.pkl")
-    st.write("Model berhasil dimuat:", model_terbaik)
-except Exception as e:
-    st.write(f"Error saat membuka file joblib: {e}")
